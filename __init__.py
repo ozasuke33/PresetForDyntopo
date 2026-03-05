a05a53d67e43179449b9957bfc3f06fc502ccadd
ozasuke33 <ozasuke33@gmail.com>
2026-03-05 22:12:28 +0900

    init

---
+++ __init__.py
@@ -0,0 +1,48 @@
+bl_info = {
+    "name": "PresetForDyntopo",
+    "author": "ozasuke",
+    "description": "Preset for Dyntopo",
+    "blender": (4, 5, 0),
+    "version": (0, 1, 0),
+    "location": "3D Vieport",
+    "warning": "",
+    "category": "Object",
+}
+
+import bpy
+
+from .preset_dyntopo import *
+
+classess = [
+    PRESET_FOR_DYNTOPO_MT_dyntopo_preset,
+    PRESET_FOR_DYNTOPO_OT_dyntopo_preset_add,
+]
+
+def preset_menu_dyntopo(self, context):
+    layout = self.layout
+
+    row = layout.row(align=True)
+    row.menu(
+        PRESET_FOR_DYNTOPO_MT_dyntopo_preset.__name__,
+        text=PRESET_FOR_DYNTOPO_MT_dyntopo_preset.bl_label,
+    )
+    row.operator(PRESET_FOR_DYNTOPO_OT_dyntopo_preset_add.bl_idname, text="", icon="ADD")
+    row.operator(
+        PRESET_FOR_DYNTOPO_OT_dyntopo_preset_add.bl_idname, text="", icon="REMOVE"
+    ).remove_active = True
+
+
+def register():
+    for cls in classess:
+        bpy.utils.register_class(cls)
+        
+    bpy.types.VIEW3D_PT_sculpt_dyntopo.prepend(preset_menu_dyntopo)    
+
+def unregister():
+    for cls in reversed(classess):
+        bpy.utils.unregister_class(cls)
+    
+    bpy.types.VIEW3D_PT_sculpt_dyntopo.remove(preset_menu_dyntopo)
+
+if __name__ == "__main__":
+    register()
---
+++ preset_dyntopo.py
@@ -0,0 +1,27 @@
+import bpy
+from bl_operators.presets import AddPresetBase
+from bl_ui.utils import PresetPanel
+
+class PRESET_FOR_DYNTOPO_MT_dyntopo_preset(bpy.types.Menu):
+    bl_label = "New Preset"
+    preset_subdir = "PresetForDyntopo/dyntopo_preset"
+    preset_operator = "script.execute_preset"
+    draw = bpy.types.Menu.draw_preset
+
+
+class PRESET_FOR_DYNTOPO_OT_dyntopo_preset_add(AddPresetBase, bpy.types.Operator):
+    bl_idname = "presetfor_dyntopo.dyntopo_preset_add"
+    bl_label = "Add Dyntopo Preset"
+    preset_menu = "PRESET_FOR_DYNTOPO_MT_dyntopo_preset"
+
+    preset_defines = ["sculpt = bpy.context.scene.tool_settings.sculpt"]
+
+    preset_values = [
+        "sculpt.constant_detail_resolution",
+        "sculpt.detail_size",
+        "sculpt.detail_percent",
+        "sculpt.detail_refine_method",
+        "sculpt.detail_type_method"
+    ]
+
+    preset_subdir = "PresetForDyntopo/dyntopo_preset"
